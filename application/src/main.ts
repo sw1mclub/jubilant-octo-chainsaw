829510074a5ae4b6f7dabd47861edaebb5e4f337
sw1mclub <sw1mclub21m@gmail.com>
2021-04-10 15:59:07 -0700

    Make uniswap always use mainnet

--- application/src/main.ts
+++ application/src/main.ts
@@ -5,7 +5,7 @@ import * as ethers from 'ethers';
 
 const provider = config.testNodeAddress;// testing for now. use `config.ethNodeAddress;` for production
 const web3Provider = new Web3.providers.HttpProvider(provider);
-const ethersProvider = new ethers.providers.JsonRpcProvider(provider);
+const ethersProvider = new ethers.providers.JsonRpcProvider(config.ethNodeAddress);
 const web3 = new Web3(web3Provider);
 web3.eth.defaultAccount = config.walletAddress;
 
--- application/src/uniswap.ts
+++ application/src/uniswap.ts
@@ -2,17 +2,19 @@ import { ChainId, Fetcher, WETH, Route, Trade, TokenAmount, TradeType } from '@u
 import ethers from 'ethers';
 import * as config from '../configs/secret-config.json';
 
-async function queryUniswap(customHttpProvider: ethers.providers.JsonRpcProvider) {
-    const chainID = config.chainID as ChainId;
+async function queryUniswap(customHttpProvider: ethers.providers.JsonRpcProvider): Promise<Trade> {
+    const chainID = ChainId.MAINNET;
     const weth = await Fetcher.fetchTokenData(chainID, WETH[chainID].address, customHttpProvider);
     const usdc = await Fetcher.fetchTokenData(chainID, config.usdcTokenAddress, customHttpProvider);
-    const pair = await Fetcher.fetchPairData(usdc, weth, customHttpProvider);
-    const route = new Route([pair], weth);
-    // const trade = new Trade(route, new TokenAmount(weth, '1'), TradeType.EXACT_INPUT);
+    const pair = await Fetcher.fetchPairData(weth, usdc, customHttpProvider);
+    const route = new Route([pair], weth, usdc);
+    
     console.log("WETH --> USDC:", route.midPrice.toSignificant(6));
     console.log("USDC --> WETH:", route.midPrice.invert().toSignificant(6));
-    // console.log("Execution Price WETH --> USDC:", trade.executionPrice.toSignificant(6));
-    // console.log("Mid Price after trade WETH --> USDC:", trade.nextMidPrice.toSignificant(6));
+    const trade = new Trade(route, new TokenAmount(weth, '100000000000000000'), TradeType.EXACT_INPUT);
+    console.log("Execution Price WETH --> USDC:", trade.executionPrice.toSignificant(6));
+    console.log("Mid Price after trade WETH --> USDC:", trade.nextMidPrice.toSignificant(6));
+    return trade;
 }
 
 export default queryUniswap;
