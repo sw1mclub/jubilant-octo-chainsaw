f842c599ec8d7b70e2bf040a68b6e2155e635b2b
sw1mclub <sw1mclub21m@gmail.com>
2021-04-11 10:39:27 -0700

    Extract strategy logic from simulation

--- application/src/simulate_strategy.ts
+++ application/src/simulate_strategy.ts
@@ -2,6 +2,7 @@ import * as config from '../configs/secret-config.json';
 import Web3 from 'web3';
 import TradeBuilder from './uniswap';
 import * as ethers from 'ethers';
+import Strategy from './strategy';
 
 const provider = config.testNodeAddress;// testing for now. use `config.ethNodeAddress;` for production
 const web3Provider = new Web3.providers.HttpProvider(provider);
@@ -9,17 +10,11 @@ const ethersProvider = new ethers.providers.JsonRpcProvider(config.ethNodeAddres
 const web3 = new Web3(web3Provider);
 web3.eth.defaultAccount = config.walletAddress;
 
-const TRADE_COOLDOWN_TIME = 1000*60*10;
-const INTERVAL = 1000*60;
+const TRADE_COOLDOWN_TIME = 1000 * 60 * 10;
+const INTERVAL = 1000 * 60;
 const VOLATILITY_THRESHOLD = 0.05;
 
-async function sleep(ms:number) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  } 
-
-async function testStrategy() {
+async function startTest() {
     const myAddress = web3.eth.defaultAccount;
     if (!myAddress) {
         console.log("ERROR: couldn't find my wallet address.");
@@ -27,40 +22,32 @@ async function testStrategy() {
     }
     const myBalanceWei = await web3.eth.getBalance(myAddress);
     const myBalance = web3.utils.fromWei(myBalanceWei, 'ether')
-    const prices = await TradeBuilder.getPrices(ethersProvider);
-    let lastEthPrice = Number(prices.wethToUSDC);
     let ethBalance = Number(myBalance);
     let usdcBalance = 0;
-    let lastTradeUnixTime = 0;
 
-    console.log("Initial eth balance: " + ethBalance);
-    while (true) {
-        console.log("CURRENT STATE:");
-        console.log({ethBalance, usdcBalance, lastTradeUnixTime});
-        await sleep(INTERVAL);
-        const now = Date.now();
-        if (lastTradeUnixTime + TRADE_COOLDOWN_TIME > now) {
-            continue;
-        }
-
+    const buyEth = async (usdcAmount: number) => {
         const prices = await TradeBuilder.getPrices(ethersProvider);
         const currentEthPrice = Number(prices.wethToUSDC);
-        const sellThreshold = lastEthPrice *  (1 + VOLATILITY_THRESHOLD);
-        const buyThreshold = lastEthPrice *  (1 - VOLATILITY_THRESHOLD);
-        if (ethBalance > 0.1 && currentEthPrice > sellThreshold) {
-            ethBalance -= ethBalance;
-            usdcBalance += ethBalance * currentEthPrice;
-            lastTradeUnixTime = Date.now();
-            continue;
-        }
-
-        if (usdcBalance > 500 && currentEthPrice < buyThreshold) {
-            ethBalance += usdcBalance / currentEthPrice;
-            usdcBalance -= usdcBalance;
-            lastTradeUnixTime = Date.now();
-            continue;
-        }
+        ethBalance += usdcAmount / currentEthPrice;
+        usdcBalance -= usdcAmount;
+        return {
+            eth: ethBalance,
+            usdc: usdcBalance
+        };
     }
+
+    const buyUSDC = async (ethAmount: number) => {
+        const prices = await TradeBuilder.getPrices(ethersProvider);
+        const currentEthPrice = Number(prices.wethToUSDC);
+        ethBalance -= ethAmount;
+        usdcBalance += ethAmount * currentEthPrice;
+        return {
+            eth: ethBalance,
+            usdc: usdcBalance
+        };
+    }
+
+    Strategy.executeStrategy(web3, ethersProvider, buyEth, buyUSDC);
 }
 
-testStrategy();
+startTest();
---
+++ application/src/strategy.ts
@@ -0,0 +1,63 @@
+import Web3 from 'web3';
+import TradeBuilder from './uniswap';
+import * as ethers from 'ethers';
+
+const TRADE_COOLDOWN_TIME = 1000 * 60 * 10;
+const INTERVAL = 1000 * 60;
+const VOLATILITY_THRESHOLD = 0.05;
+
+type BuyFunc = (amount: number) => Promise<{ eth: number, usdc: number }>;
+
+async function sleep(ms: number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+async function executeStrategy(web3: Web3, ethersProvider: ethers.providers.JsonRpcProvider, buyEth: BuyFunc, buyUSDC: BuyFunc) {
+    const myAddress = web3.eth.defaultAccount;
+    if (!myAddress) {
+        console.log("ERROR: couldn't find my wallet address.");
+        return null;
+    }
+    const myBalanceWei = await web3.eth.getBalance(myAddress);
+    const myBalance = web3.utils.fromWei(myBalanceWei, 'ether')
+    const prices = await TradeBuilder.getPrices(ethersProvider);
+    let lastEthPrice = Number(prices.wethToUSDC);
+    let ethBalance = Number(myBalance);
+    let usdcBalance = 0;
+    let lastTradeUnixTime = 0;
+
+    console.log("Initial eth balance: " + ethBalance);
+    while (true) {
+        console.log("CURRENT STATE:");
+        console.log({ ethBalance, usdcBalance, lastTradeUnixTime });
+        await sleep(INTERVAL);
+        const now = Date.now();
+        if (lastTradeUnixTime + TRADE_COOLDOWN_TIME > now) {
+            continue;
+        }
+
+        const prices = await TradeBuilder.getPrices(ethersProvider);
+        const currentEthPrice = Number(prices.wethToUSDC);
+        const sellThreshold = lastEthPrice * (1 + VOLATILITY_THRESHOLD);
+        const buyThreshold = lastEthPrice * (1 - VOLATILITY_THRESHOLD);
+        if (ethBalance > 0.1 && currentEthPrice > sellThreshold) {
+            const result = await buyUSDC(ethBalance);
+            ethBalance = result.eth;
+            usdcBalance = result.usdc;
+            lastTradeUnixTime = Date.now();
+            continue;
+        }
+
+        if (usdcBalance > 500 && currentEthPrice < buyThreshold) {
+            const result = await buyEth(usdcBalance);
+            ethBalance = result.eth;
+            usdcBalance = result.usdc;
+            lastTradeUnixTime = Date.now();
+            continue;
+        }
+    }
+}
+
+export default { executeStrategy };
